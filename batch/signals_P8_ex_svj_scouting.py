a8c029e90d58c894adad46f2b83fce6bc1ea3f71
Marcel Gaisdörfer <uhvtp@student.kit.edu>
2025-01-17 06:01:25 -0600

    added scouting configs

---
+++ batch/signals_P8_ex_svj_scouting.py
@@ -0,0 +1,17 @@
+flist = [
+    {"channel": "s", "mMediator": 1100, "mDark": 20, "rinv": 0.3, "alpha": "peak", "scout": True},
+]
+
+
+'''
+flist = []
+
+mPseudo = 8.0
+mVector = 15
+for MZp in [3000]:
+        for rinv in [0.3]:
+          for brgamma in [0.3]:       
+            for mPiOverLambda in [1.0]:     
+                 for lambdaHV in [2.0]: 
+                        flist.append({"channel": "s", "svjgamma": 1 , "mMediator": MZp, "mPseudo": mPseudo,"mVector": mVector,"rinv": rinv, "alpha": "peak", "lambdaHV": lambdaHV, "mPiOverLambda": mPiOverLambda, "BRGamma": brgamma })
+'''
