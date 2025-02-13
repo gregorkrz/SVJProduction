9e676f7849551127f50a7e6fb0a2fe6f4fc1319f
Gregor Kržmanc <gregor.krzmanc@gmail.com>
2025-02-13 15:34:55 +0100

    update prodconfig.sh

--- batch/prodconfig.sh
+++ batch/prodconfig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 YEAR=2018
-NJOBS=75
+NJOBS=20
 
 (set -x
-python runProd.py -P P8 -G="-p -d signals_P8_ex_svj_scouting -E 1500 -N 3000 --cpus 4 --memory 8000 --args 'quiet=1 syst=1'" -y 2018 -n chain2018_ -o root://cmsdcache-kit-disk.gridka.de:1094//store/user/mgaisdor/SVJProduction/SVJScouting_UL18_private_prod2/ -t root://cmsdcache-kit-disk.gridka.de:1094//store/user/mgaisdor/SVJProduction/SVJScouting_UL18_private_prod2/ -c -s
+python runProd.py -P P8 -G="-p -d signals_P8_ex_svj_scouting -E 10 -N 10 --cpus 4 --memory 8000 --args 'quiet=1 syst=1'" -y 2018 -n chain2018_ -o root://t3se01.psi.ch:1094/store/user/gkrzmanc/jetclustering/sim/test_13feb  -t root://t3se01.psi.ch:1094/store/user/gkrzmanc/jetclustering/sim/test_13feb_tarball -c -s
 )
