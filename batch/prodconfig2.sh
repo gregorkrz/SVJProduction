854d201aa64885a1f641bc4b672eddd443409f6a
Gregor Krzmanc <gregor.krzmanc@cern.ch>
2025-02-26 14:29:43 +0100

    add prodconfig2.sh

---
+++ batch/prodconfig2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+YEAR=2018
+NJOBS=10
+
+(set -x
+python runProd.py -P P8 -G="-p -d signals_P8_ex_svj_scouting -E 10 -N 10 --cpus 4 --memory 8000 --args 'quiet=1 syst=1'" -y 2018 -n chain2018_ -o root://t3se01.psi.ch:1094/store/user/gkrzmanc/jetclustering/sim/26feb  -t root://t3se01.psi.ch:1094/store/user/gkrzmanc/jetclustering/sim/tb_26feb -c -s
+)
+
