7cfc704f5f6a7fc985e0584c4aa971df06d1dada
Gregor Krzmanc <gregor.krzmanc@cern.ch>
2025-02-26 16:11:10 +0100

    fix bug

--- batch/prodconfig2_modified_selection_crit.sh
+++ batch/prodconfig2_modified_selection_crit.sh
@@ -4,5 +4,5 @@ YEAR=2018
 NJOBS=10
 
 (set -x
-python runProd.py -P P8 -G="-p -d signals_P8_ex_svj_scouting -E 10 -N 10 --cpus 4 --memory 8000 --args 'quiet=1 syst=1'" -y 2018 -n chain2018_ -o root://t3se01.psi.ch:1094/store/user/gkrzmanc/jetclustering/sim/26febMS  -t root://t3se01.psi.ch:1094/store/user/gkrzmanc/jetclustering/sim/tb_26febMS -c -s
+python runProd.py -P P8 -G="-p -d signals_P8_ex_svj_scouting -E 10 -N 10 --cpus 4 --memory 8000 --args 'quiet=1 syst=1'" -y 2018 -n chain2018_ -o root://t3se01.psi.ch:1094/store/user/gkrzmanc/jetclustering/sim/26febMS1  -t root://t3se01.psi.ch:1094/store/user/gkrzmanc/jetclustering/sim/tb_26febMS1 -c -s
 )
